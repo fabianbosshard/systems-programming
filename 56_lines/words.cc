7745944fa3fb517dd5ee01ce6b3d3b03ad9871f5
Fabian Bosshard <fabianlucasbosshard@gmail.com>
2025-08-31 15:56:14 +0200

    switch to strictly monotonic IDs => an ID is never reused...

--- 56_lines/words.cc
+++ 56_lines/words.cc
@@ -6,13 +6,14 @@
 
 #include "words.h"
 
-
 struct lines {
     std::map<unsigned, std::string> M;
+    unsigned next_id;
 };
 
 struct lines * lines_create() {
     struct lines * l = new struct lines;
+    l->next_id = 0;
     return l;
 }
 
@@ -21,13 +22,7 @@ void lines_destroy (struct lines * l) {
 }
 
 int add_line (struct lines * l, const char * line) {
-    unsigned id;
-    auto last = l->M.rbegin();
-    if (last == l->M.rend()) {
-        id = 0;
-    } else {
-        id = last->first + 1;
-    }
+    unsigned id = l->next_id++;
     l->M[id] = std::string(line);
     return id;
 }
@@ -40,36 +35,18 @@ int remove_line (struct lines * l, int id) {
     return true;
 }
 
-bool match(std::string X, std::set<std::string> W) {
-    if (W.empty()) {
-        return false; // empty set of words never matches a string...
-    }
-    for (auto w : W) {
-        if (X.find(w) == std::string::npos) { // X does not contain w as a substring
-            return false;
-        }
-    }
-    return true;
-}
-
-
 const char * get_word_start(const char * p) {
-    while (isspace(*p) || *p == '\0') {
-        if (*p == '\0') {
-            return NULL; // no more words in p
-        }
+    while (isspace(*p) && *p != '\0') {
         p++;
     }
-    return p;
+    return (*p) ? p : NULL; // if *p is the string terminator '\0', there are no more words in p...
 }
-
 const char * get_word_end(const char * q) {
     while (!isspace(*q) && *q != '\0') {
         q++;
     }
     return q;
 }
-
 std::set<std::string> parse_words(const char * words) {
     const char * p = words;
     const char * q = words;
@@ -82,7 +59,15 @@ std::set<std::string> parse_words(const char * words) {
     }
     return W;
 }
-
+bool match(std::string X, std::set<std::string> W) {
+    if (W.empty()) return false; // empty set of words never matches a string...
+    for (auto w : W) {
+        if (X.find(w) == std::string::npos) { // X does not contain w as a substring
+            return false;
+        }
+    }
+    return true;
+}
 int match (struct lines * l, const char * output[], int max_output, const char * words) {
     unsigned i = 0;
     std::set<std::string> W = parse_words(words);
